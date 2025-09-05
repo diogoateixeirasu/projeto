1db814634098e321f1b26366f753f8002dfcf117
Diogo <diogoateixeira@gmail.com>
2025-09-05 23:24:18 +0100

    :tada:

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // buscar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem ligh
+    img.setAttribute("src", "./assets/avatar2.png")
+  } else {
+    //se nao tiver light mode, mantem a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+  //se nao tiver imagem, o que contem a imagem
+  if (html.classList.contains("light")) {
+    img.setAttribute("alt", "imagem de suski azul")
+  } else {
+    img.setAttribute("alt", "imagem de suski vermelha")
+  }
+}
