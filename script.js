d824f16ed0788811de387f9685056bd47d87b3e6
Diogo <diogoateixeira@gmail.com>
2025-09-06 04:04:43 +0100

    changing the alt on image

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem ligh
-    img.setAttribute("src", "./assets/avatar2.png")
+    img.setAttribute("src", "./assets/Avatar2.png")
+    img.setAttribute("alt", "imagem de suski light")
   } else {
     //se nao tiver light mode, mantem a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Avatar.png")
+    img.setAttribute("alt", "imagem de suski dark")
   }
 }
