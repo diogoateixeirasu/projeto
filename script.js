07f0946c6e91f740b58bdfa1b7d11b4249d4bc26
Diogo <diogoateixeira@gmail.com>
2025-09-06 03:33:34 +0100

    change image

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function toggleMode() {
   } else {
     //se nao tiver light mode, mantem a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute("alt", "imagem dark")
   }
+
+  //se nao aparecer imagem
 }
